a23e7c35c34798dd54c8e54fa50b936eeb6e1dfa
ovielee <anthony.oke@outlook.com>
2025-01-23 22:29:12 +0000

    Update Vercel configuration

--- server.js
+++ server.js
@@ -5,5 +5,5 @@ const PORT = process.env.PORT || 3000;
 
 // Start the server
 app.listen(PORT, () => {
-  console.log(`Server running on http://localhost:${PORT}`);
+  console.log(`Server running on port ${PORT}`);
 });
