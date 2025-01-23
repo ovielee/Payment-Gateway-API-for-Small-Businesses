c276a4d3b01ae12def0118688200492be94ecc24
ovielee <anthony.oke@outlook.com>
2025-01-23 20:02:31 +0000

    4th C

--- server.js
+++ server.js
@@ -1,7 +1,7 @@
 const app = require('./app'); // Import your Express app
 
 // Set the port (use the environment variable PORT or default to 4000)
-const PORT = process.env.PORT || 4000;
+const PORT = process.env.PORT || 3000;
 
 // Start the server
 app.listen(PORT, () => {
